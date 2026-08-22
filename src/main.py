55e271c8e0d717691a1a757519b8e1e876e3742a
Vara112 <elliottvara@gmail.com>
2026-08-22 23:51:27 +1000

    Testing threshold

--- src/main.py
+++ src/main.py
@@ -3,12 +3,45 @@ import cv2
 import numpy as np
 
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-cap.set(cv2.CAP_PROP_FPS, 120)
+
+def threshold_test(vidPtr):
+    cv2.namedWindow('controls')
+    cv2.createTrackbar('thresh', 'controls', 50, 255, lambda x: None)
+
+    while True:
+        ret, frame = vidPtr.read()
+
+        if not ret:
+            #if frame was failed to be pulled
+            print("Error pulling frame")
+            break
+
+        #Cam is upside down TODO
+        #frame = cv2.rotate(frame, cv2.ROTATE_180)
+
+        threshVal = cv2.getTrackbarPos('thresh', 'controls') #Takes the value from slider
+        _, mask = cv2.threshold(frame, threshVal, 255, cv2.THRESH_BINARY_INV)
+
+        cv2.imshow('frame', frame)
+        cv2.imshow('mask', mask)
+
+        key = cv2.waitKey(30) & 0xFF 
+        if key == ord('q'):
+            vidPtr.release()
+            cv2.destroyAllWindows()
+            exit()
 
 
-cv2.namedWindow('controls')
-cv2.createTrackbar('thresh', 'controls', 50, 255, lambda x: None)
+if __name__ == "__main__":
+    '''
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    cap.set(cv2.CAP_PROP_FPS, 120)
+    '''
+
+
+    cap = cv2.VideoCapture('vids/normal.avi')
+
+    threshold_test(cap)
