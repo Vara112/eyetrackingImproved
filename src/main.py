68a5bb1a62dd5631a88563a08ba99d49b5ce87a6
Vara112 <elliottvara@gmail.com>
2026-08-23 00:03:22 +1000

    Edge detection for pupil

--- src/main.py
+++ src/main.py
@@ -20,10 +20,23 @@ def threshold_test(vidPtr):
         #frame = cv2.rotate(frame, cv2.ROTATE_180)
 
         threshVal = cv2.getTrackbarPos('thresh', 'controls') #Takes the value from slider
+        #50 seems to work the best
+        
+        #print(threshVal)
         _, mask = cv2.threshold(frame, threshVal, 255, cv2.THRESH_BINARY_INV)
+
+
+
+        #Edge finding
+
+        edges = cv2.Canny(mask, 0, 255, 3)  #3 is default (aperturesize not sure what that does)
+
+
+
 
         cv2.imshow('frame', frame)
         cv2.imshow('mask', mask)
+        cv2.imshow('edges', edges)
 
         key = cv2.waitKey(30) & 0xFF 
         if key == ord('q'):
