1ea1f767b323d954ff4cae04e5a271f68b84e265
Vara112 <elliottvara@gmail.com>
2026-08-23 00:25:16 +1000

    calcs the largest contour, applies spatial noise rejection, and then draws ellipse around pupil

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@
 import cv2
 import numpy as np
 
-
+THRESHOLD_VAL = 65
 
 def threshold_test(vidPtr):
-    cv2.namedWindow('controls')
-    cv2.createTrackbar('thresh', 'controls', 50, 255, lambda x: None)
+    #cv2.namedWindow('controls')
+    #cv2.createTrackbar('thresh', 'controls', 50, 255, lambda x: None)
 
     while True:
         ret, frame = vidPtr.read()
@@ -19,12 +19,16 @@ def threshold_test(vidPtr):
         #Cam is upside down TODO
         #frame = cv2.rotate(frame, cv2.ROTATE_180)
 
-        threshVal = cv2.getTrackbarPos('thresh', 'controls') #Takes the value from slider
+        #threshVal = cv2.getTrackbarPos('thresh', 'controls') #Takes the value from slider
         #50 seems to work the best
-        
-        #print(threshVal)
-        _, mask = cv2.threshold(frame, threshVal, 255, cv2.THRESH_BINARY_INV)
 
+        #print(threshVal)
+        _, mask = cv2.threshold(frame, THRESHOLD_VAL, 255, cv2.THRESH_BINARY_INV)
+
+
+        #Dark shadow in corner causing issues, so 0 the top right so contour doesnt pick it up
+        h, w = mask.shape[:2]
+        mask[0:100, w-100:w] = 0
 
 
         #Edge finding
@@ -32,9 +36,27 @@ def threshold_test(vidPtr):
         edges = cv2.Canny(mask, 0, 255, 3)  #3 is default (aperturesize not sure what that does)
 
 
+        contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        #Not sure if hierarchy is needed. Maybe for some filtering later?
+
+        dispFrame = frame.copy()
+
+        if contours:
+            #Theoretically the largest contour will be the pupil
+            largestContour = max(contours, key=cv2.contourArea)
+
+            #Tries to smooth out noise (spatial noise rejection)
+            M = cv2.moments(largestContour)
+            if M['m00'] != 0:
+                cx = int(M['m10'] / M['m00'])
+                cy = int(M['m01'] / M['m00'])
+
+                cv2.drawContours(dispFrame, [largestContour], -1, (255, 0, 0), 2)
+                cv2.circle(dispFrame, (cx, cy), 5, (0, 0, 255), -1)
 
 
-        cv2.imshow('frame', frame)
+
+        cv2.imshow('frame', dispFrame)
         cv2.imshow('mask', mask)
         cv2.imshow('edges', edges)
 
